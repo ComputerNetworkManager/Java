fd99823b8fe5901fc72e77af57d78902a31333fc
Lian <cheesetastisch@gmail.com>
2022-09-27 22:32:10 +0200

    Removed mapping

--- SharedAPI/src/main/java/me/cnm/shared/utility/IUtilityHandler.java
+++ SharedAPI/src/main/java/me/cnm/shared/utility/IUtilityHandler.java
@@ -1,19 +1,9 @@
 package me.cnm.shared.utility;
 
-import me.cnm.shared.utility.mapping.ITypeMapper;
-import org.jetbrains.annotations.NotNull;
-
 /**
  * Class to expose utility resources
  */
 public interface IUtilityHandler {
 
-    /**
-     * Get the type mapper
-     * @return the type mapper
-     * @see ITypeMapper
-     */
-    @NotNull
-    ITypeMapper getTypeMapper();
 
 }
